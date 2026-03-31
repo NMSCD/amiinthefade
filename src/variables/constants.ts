97d97f12c7f035af564d2ff2a5cadf7851a94252
Lenni009 <guentherhanslphdyt@gmail.com>
2026-03-31 20:54:16 +0200

    remove unused variables

--- src/variables/constants.ts
+++ src/variables/constants.ts
@@ -1,5 +1,3 @@
-export const systemIndexLength = 3;
 export const validInputRegex = /[0-9A-F:]/;
 export const maxGlyphLength = 12;
 export const maxCoordLength = 19;
-export const eisvanaRegionAmount = 7;
